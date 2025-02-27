c2b14f59176416814152231375ef27313e105577
hyunkyoun <ekim2350@gmail.com>
2025-02-27 00:18:45 -0500

    Update test_webhook.py
    added name

--- test_webhook.py
+++ test_webhook.py
@@ -6,20 +6,23 @@ import target
 import asyncio
 
 TARGET_URLS = [
+    "https://www.target.com/p/starburst-easter-original-jellybeans-14oz/-/A-53951890",
     "https://www.target.com/p/pokemon-scarlet-violet-s3-5-booster-bundle-box/-/A-88897904#lnk=sametab",
     "https://www.target.com/p/pok-233-mon-trading-card-game-zapdos-ex-deluxe-battle-deck/-/A-91351689#lnk=sametab",
-    "https://www.target.com/p/starburst-easter-original-jellybeans-14oz/-/A-53951890",
 ]
 
 TARGET_NAMES = [
+    "Starburst Easter Original Jellybeans 14oz",
     "Pokemon Scarlet & Violet S3.5 Booster Bundle Box",
     "Pokemon Trading Card Game: Zapdos-EX Deluxe Battle Deck",
-    "Starburst Easter Original Jellybeans 14oz",
 ]
 
 def send_embed(url, name, sku):
     webhook_url = "https://discord.com/api/webhooks/1344064369822138419/La7IVFunEqn2iUG9tIcSK-zFQ-QxmlZq2TXUikMK25lYZZAKSOxTw6kVL2vseqzmhXcW"
-    webhook = DiscordWebhook(url=webhook_url)
+    webhook = DiscordWebhook(
+        url=webhook_url,
+        username="Target Monitor",
+    )
 
     embed = DiscordEmbed(title="Target Stock Notification", description=f"[{name}]({url})", color=0x00FF00)
     embed.add_embed_field(name="SKU", value=f"{sku}")
