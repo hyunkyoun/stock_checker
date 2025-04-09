a246f9d8985af44dc0d2b790e81053e2f5cf451b
hyunkyoun <ekim2350@gmail.com>
2025-04-09 02:40:13 -0400

    Update target_stock_checker_api.py

--- target_stock_checker_api.py
+++ target_stock_checker_api.py
@@ -1,8 +1,20 @@
 import requests
 import uuid
+from discord_webhook import DiscordWebhook, DiscordEmbed
+from datetime import datetime
+import re
+import json
+import time
+import random
+import os
+from dotenv import load_dotenv
 
 def generate_visitor_id():
     return uuid.uuid4().hex.upper()
+
+def extract_product_id(url):
+    match = re.search(r'/A-(\d+)', url)
+    return match.group(1) if match else None
 
 def is_in_stock(tcin: str, location_id: str = "1263", scheduled_store_id: str = "3329"):
     url = "https://redsky.target.com/redsky_aggregations/v1/web/product_fulfillment_and_variation_hierarchy_v1"
@@ -43,8 +55,56 @@ def is_in_stock(tcin: str, location_id: str = "1263", scheduled_store_id: str = 
         "scheduled_delivery": scheduled_status
     }
 
-# Example usage
-print(is_in_stock("92327434"))
-print(is_in_stock("93954446"))
-print(is_in_stock("92538450"))
+def send_embed(url, name, sku, cart_type):
+    webhook_url = os.getenv("TARGET_WEBHOOK")
+    webhook = DiscordWebhook(
+        url=webhook_url,
+        username="Target Stock Monitor",
+    )
 
+    embed = DiscordEmbed(title="Target Stock Notification", description=f"[{name}]({url})", color=0x00FF00)
+    embed.add_embed_field(name="SKU", value=f"{sku}")
+
+    if cart_type == "atc":
+        embed.add_embed_field(name="Product Status", value="Available Now")
+    elif cart_type == "pre":
+        embed.add_embed_field(name="Product Status", value="Pre-Order")
+
+    embed.set_footer(text=f"{datetime.now().strftime('%H:%M:%S')}")
+
+    webhook.add_embed(embed)
+    response = webhook.execute()
+
+    if response.status_code == 200:
+        print("Message sent successfully!")
+    else:
+        print("Failed to send message. Status code: ", response.status_code)
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    with open('target_product_pages.json', 'r') as f:
+        data = json.load(f)
+    
+    target_product_pages = data['product_pages']
+
+    while True:
+        for product in target_product_pages:
+            url = product['url']
+            name = product['name']
+            sku = extract_product_id(url)
+
+            print(f"Checking {name} ({sku})...")
+
+            in_stock = is_in_stock(sku)
+
+            if in_stock['shipping'] == "IN_STOCK":
+                send_embed(url, name, sku, "atc")
+                print("item is in stock!")
+            elif in_stock['shipping'] == "PRE_ORDER_SELLABLE":
+                send_embed(url, name, sku, "pre")
+                print("item is available for pre-order!")
+            else:
+                print("item is out of stock.")
+
+            time.sleep(random.randint(5, 10))  
